97c7da992e931d685807048cfe65a2aa6a983374
gunsha <gonzaller@gmail.com>
2026-04-15 10:11:17 -0300

    Initial Commit

---
+++ VideoConverter/VideoConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoConverterApp.swift
+//  VideoConverter
+//
+//  Created by Gonzalo Aller on 15/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct VideoConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
